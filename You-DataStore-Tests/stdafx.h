9682bfde697877fdb2ebae1e724c5e735c3dcba7
Joel Low <joel@joelsplace.sg>
2014-09-16 17:59:14 +0800

    Add test projects for the rest of the components.

---
+++ You-DataStore-Tests/stdafx.h
@@ -0,0 +1,13 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef YOU_DATASTORE_TESTS_STDAFX_H_
+#define YOU_DATASTORE_TESTS_STDAFX_H_
+
+#include <SDKDDKVer.h>
+#include <CppUnitTest.h>
+
+#endif  // YOU_DATASTORE_TESTS_STDAFX_H_
---
+++ You-QueryEngine-Tests/stdafx.h
@@ -0,0 +1,13 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef YOU_QUERYENGINE_TESTS_STDAFX_H_
+#define YOU_QUERYENGINE_TESTS_STDAFX_H_
+
+#include <SDKDDKVer.h>
+#include <CppUnitTest.h>
+
+#endif  // YOU_QUERYENGINE_TESTS_STDAFX_H_
