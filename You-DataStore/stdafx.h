bd35a39091b4c3c9b7967aeadcf541e547dae0ca
Joel Low <joel@joelsplace.sg>
2014-09-16 15:31:22 +0800

    Added the data store and query engine projects.

---
+++ You-DataStore/stdafx.h
@@ -0,0 +1,12 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef YOU_DATASTORE_STDAFX_H_
+#define YOU_DATASTORE_STDAFX_H_
+
+#include <SDKDDKVer.h>
+
+#endif  // YOU_DATASTORE_STDAFX_H_
---
+++ You-QueryEngine/stdafx.h
@@ -0,0 +1,12 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef YOU_QUERYENGINE_STDAFX_H_
+#define YOU_QUERYENGINE_STDAFX_H_
+
+#include <SDKDDKVer.h>
+
+#endif  // YOU_QUERYENGINE_STDAFX_H_
