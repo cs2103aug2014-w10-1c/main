02a426b65373c9b45c9d32453774ec61f959c2f3
Joel Low <joel@joelsplace.sg>
2014-09-16 15:21:09 +0800

    Add the NLP library.

---
+++ You-NLP/stdafx.h
@@ -0,0 +1,12 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef YOU_NLP_STDAFX_H_
+#define YOU_NLP_STDAFX_H_
+
+#include <SDKDDKVer.h>
+
+#endif  // YOU_NLP_STDAFX_H_
