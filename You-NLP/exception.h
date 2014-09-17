f90420c38c29bb99953c71dd0ab2adf2ec7ac73f
Joel Low <joel@joelsplace.sg>
2014-09-17 14:46:44 +0800

    Define a base exception type.

---
+++ You-NLP/exception.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef YOU_NLP_EXCEPTION_H_
+#define YOU_NLP_EXCEPTION_H_
+
+#include <stdexcept>
+
+namespace You {
+namespace NLP {
+	class Exception : public std::exception {
+	};
+}
+}
+
+#endif  // YOU_NLP_EXCEPTION_H_
