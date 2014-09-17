b30ae6c98721a8405b86b773316189da229ebc4f
Joel Low <joel@joelsplace.sg>
2014-09-17 11:58:53 +0800

    Define the result set class.

---
+++ You-NLP/result.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef YOU_NLP_RESULT_H_
+#define YOU_NLP_RESULT_H_
+
+namespace You {
+namespace NLP {
+	/// Defines the result of a query.
+	class Result {
+
+	protected:
+		union {
+
+		};
+	};
+}
+}
+
+#endif
