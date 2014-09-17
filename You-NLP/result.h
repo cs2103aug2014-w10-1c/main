05a7739b1a86e25b85b95332cc59605f99c2c377
Joel Low <joel@joelsplace.sg>
2014-09-17 15:15:03 +0800

    Add missing accessors.

--- You-NLP/result.h
+++ You-NLP/result.h
@@ -15,15 +15,33 @@ public:
 	/// \return true if the result is a result set result.
 	bool isResultSetResult() const;
 
+	/// Gets the result set if the current result is one.
+	///
+	/// \return The result set if this is a result set result. Behaviour is
+	///         undefined otherwise.
+	const TaskList& getResultSet() const;
+
 	/// Checks if this result is a task object.
 	///
 	/// \return true if the result is an object result.
 	bool isAffectedObjectResult() const;
 
+	/// Gets the affected object if the current result is one.
+	///
+	/// \return The affected object if this is an affected object result.
+	///         Behaviour is undefined otherwise.
+	const Task& getAffectedObject() const;
+
 	/// Checks if this result is a affected row count.
 	///
 	/// \return true if the result is an affected row count result.
 	bool isAffectedCountResult() const;
+
+	/// Gets the affected object count if the current result is one.
+	///
+	/// \return The number of affected objects if this is an affected object
+	///         count result. Behaviour is undefined otherwise.
+	const size_t getAffectedCount() const;
 
 private:
 	/// The possible types of results.
