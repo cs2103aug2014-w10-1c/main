52c888cb3c7dbff93c6034f666f2d0b64fbd149f
Joel Low <joel@joelsplace.sg>
2014-09-19 08:52:38 +0800

    Use Boost's default synthesised attribute handling for the implicit conversion to QUERY.

--- You-NLP/internal/parser.cpp
+++ You-NLP/internal/parser.cpp
@@ -27,10 +27,10 @@ QUERY Parser::parse(const Parser::StringType& string) {
 }
 
 Parser::Parser() : Parser::base_type(start) {
-	start = (
+	start %= (
 		(qi::lit(L"/") >> explicitCommand) |
 		addCommand
-	)[qi::_val = phoenix::bind(&Parser::combineStart, qi::_1);
+	);
 
 	explicitCommand %=
 		(qi::lit(L"add") >> addCommand) |
@@ -39,20 +39,6 @@ Parser::Parser() : Parser::base_type(start) {
 	addCommand = (
 		qi::lexeme[+ParserCharTraits::char_]
 	)[qi::_val = phoenix::bind(&Parser::constructAddQuery, qi::_1)];
-}
-
-QUERY Parser::combineStart(boost::variant<QUERY, ADD_QUERY>& nonterminal) {
-	struct StartVisitor : public boost::static_visitor<QUERY> {
-		QUERY operator()(QUERY& q) {
-			return q;
-		}
-
-		QUERY operator()(ADD_QUERY& q) {
-			return q;
-		}
-	} visitor;
-
-	return boost::apply_visitor(visitor, nonterminal);
 }
 
 ADD_QUERY Parser::constructAddQuery(const LexemeType& lexeme) {
