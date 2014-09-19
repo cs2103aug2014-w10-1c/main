8e7ce65ca90d26d14edbe2961b0894faff5fa44a
Joel Low <joel@joelsplace.sg>
2014-09-19 09:54:09 +0800

    Fix lint warnings.

--- You-NLP/internal/parse_tree/add_query.h
+++ You-NLP/internal/parse_tree/add_query.h
@@ -9,31 +9,31 @@ namespace You {
 namespace NLP {
 namespace Internal {
 
-	/// Defines the synthesised value type of an add query.
-	struct ADD_QUERY {
-		/// Equality comparator.
-		///
-		/// \param[in] rhs The other query object to compare with.
-		bool operator==(const ADD_QUERY& rhs) const;
-
-		/// String conversion -- mainly useful for unit testing and display.
-		///
-		/// \return The string representation of the current query object.
-		std::wstring toString() const;
-
-		/// The description of the task.
-		std::wstring description;
-
-		/// The due date of the task.
-		std::wstring due;
-	};
-
-	/// Defines an output formatter for ADD_QUERY queries.
+/// Defines the synthesised value type of an add query.
+struct ADD_QUERY {
+	/// Equality comparator.
 	///
-	/// \param[in,out] s The stream to write to.
-	/// \param[in] q The query to display.
-	/// \return The stream object passed to the formatter.
-	std::wostream& operator<<(std::wostream& s, const ADD_QUERY& q);
+	/// \param[in] rhs The other query object to compare with.
+	bool operator==(const ADD_QUERY& rhs) const;
+
+	/// String conversion -- mainly useful for unit testing and display.
+	///
+	/// \return The string representation of the current query object.
+	std::wstring toString() const;
+
+	/// The description of the task.
+	std::wstring description;
+
+	/// The due date of the task.
+	std::wstring due;
+};
+
+/// Defines an output formatter for ADD_QUERY queries.
+///
+/// \param[in,out] s The stream to write to.
+/// \param[in] q The query to display.
+/// \return The stream object passed to the formatter.
+std::wostream& operator<<(std::wostream& s, const ADD_QUERY& q);
 
 }  // namespace Internal
 }  // namespace NLP
@@ -45,4 +45,4 @@ BOOST_FUSION_ADAPT_STRUCT(
 	(std::wstring, due)
 )
 
-#endif
+#endif  // YOU_NLP_INTERNAL_PARSE_TREE_ADD_QUERY_H_
