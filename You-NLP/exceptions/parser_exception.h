65eee8f6c948cc08e6cb045a04ff86dbf3343b2b
Joel Low <joel@joelsplace.sg>
2014-09-19 09:48:39 +0800

    Implement a base parse exception type.

---
+++ You-NLP/exceptions/parser_exception.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef YOU_NLP_EXCEPTIONS_PARSER_EXCEPTION_H_
+#define YOU_NLP_EXCEPTIONS_PARSER_EXCEPTION_H_
+
+#include "../exception.h"
+
+namespace You {
+namespace NLP {
+
+class ParserException : public Exception {
+};
+
+}  // namespace NLP
+}  // namespace You
+
+#endif  // YOU_NLP_EXCEPTIONS_PARSER_EXCEPTION_H_
