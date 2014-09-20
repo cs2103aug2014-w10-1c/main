16fbcf4195985f702a436d2e0960e72a9fd2ec54
Diga Widyaprana <digawp@gmail.com>
2014-09-20 15:23:39 +0800

    Add skeleton of for DataStore

---
+++ You-DataStore/Storage.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "Storage.h"
+
+namespace You {
+namespace DataStore {
+
+} // namespace DataStore
+} // namespace You
---
+++ You-DataStore/Storage.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef YOU_DATASTORE_STORAGE_H_
+#define YOU_DATASTORE_STORAGE_H_
+
+namespace You {
+namespace DataStore {
+
+class Storage {
+public:
+	Storage() = default;
+	~Storage() = default;
+};
+
+} // namespace DataStore
+} // namespace You
+
+#endif // YOU_DATASTORE_STORAGE_H_
