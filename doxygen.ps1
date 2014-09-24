2341d21504bab59a5e6b38636f3875375de57545
Joel Low <joel@joelsplace.sg>
2014-09-24 11:59:33 +0800

    Add experimental support to push to GitHub pages for documentation.

--- doxygen.ps1
+++ doxygen.ps1
@@ -38,3 +38,36 @@ if ($doxygen_exists -ne $true) {
 if ($LastExitCode -ne 0) {
 	Throw "doxygen completed with errors."
 }
+
+$documentation_dir = 'docs'
+$github_pages_dir = 'you_documentation'
+if ($env:YOU_DOCUMENTATION_KEY) {
+	$ssh_config_dir = (Resolve-Path '~/.ssh')[0].Path
+	mkdir $ssh_config_dir
+
+	$key = [System.Convert]::FromBase64String($env:YOU_DOCUMENTATION_KEY)
+	[System.IO.File]::WriteAllBytes($ssh_config_dir + '\\id_rsa', $key)
+
+	echo 'Host github.com
+	HostName github.com
+	User git
+	IdentityFile ' + $ssh_config_dir + '\\id_rsa' |
+	Add-Content ($ssh_config_dir + '/config')
+
+	&git clone git@github.com:cs2103aug2014-w10-1c/you-documentation.git $github_pages_dir --quiet 2>&1 | %{
+		if ($_.gettype().Name -eq "ErrorRecord") {
+			$Host.UI.WriteErrorLine($_)
+		} else {
+			echo $_
+		}
+	}
+
+	Remove-Item $github_pages_dir -Exclude *.git -Recurse
+	pushd $github_pages_dir
+	move ('..\' + $documentation_dir + '\html\*') '.'
+	&git add --all
+	&git commit -m 'Latest documentation'
+	&git push
+
+	popd
+}
