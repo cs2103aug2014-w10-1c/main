dfb4edd6de900177efa9d90cbf719bf9166a88d2
Evan Sebastian <evans@comp.nus.edu.sg>
2014-09-24 16:49:22 +0800

    Implement some functionality in query.cpp

--- You-QueryEngine/query.cpp
+++ You-QueryEngine/query.cpp
@@ -5,5 +5,27 @@
 
 namespace You {
 namespace QueryEngine {
+
+std::shared_ptr<Query> AddTask(Task::Description description
+	, Task::Time deadline
+	, Task::Priority priority
+	, Task::Dependencies dependencies
+	) {
+	using Internal::Query::AddTask;
+	return std::make_shared<AddTask>(description, deadline,
+		priority, dependencies);
+}
+
+std::shared_ptr<Query> GetTask(Task::ID id) {
+	using Internal::Query::GetTask;
+	return std::make_shared<GetTask>(id);
+}
+
+std::shared_ptr<Query> DeleteTask(Task::ID taskID) {
+	using Internal::Query::DeleteTask;
+	return std::make_shared<DeleteTask>(taskID);
+}
+
 }  // namespace QueryEngine
 }  // namespace You
+
