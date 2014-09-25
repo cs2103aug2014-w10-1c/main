f977a728bd15243653984ff075a5deda08e96118
Diga Widyaprana <digawp@gmail.com>
2014-09-26 01:08:01 +0800

    Created IOperation interface for query engine

---
+++ You-DataStore/ioperation.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef YOU_DATASTORE_IOPERATION_H_
+#define YOU_DATASTORE_IOPERATION_H_
+
+namespace You {
+namespace DataStore {
+
+/// A pure virtual class of operations to be put into transaction stack
+class IOperation {
+public:
+	IOperation() = default;
+	~IOperation() = default;
+
+	/// Executes the operation
+	virtual void run() = 0;
+
+};
+
+}   // namespace DataStore
+}   // namespace You
+
+#endif  // YOU_DATASTORE_IOPERATION_H_
