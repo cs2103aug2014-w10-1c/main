8d900c4104e8dd7bee1c1d697831989fb1463b51
Evan Sebastian <evans@comp.nus.edu.sg>
2014-09-25 10:43:35 +0800

    Refactor query_tests.cpp

--- You-QueryEngine-Tests/query_tests.cpp
+++ You-QueryEngine-Tests/query_tests.cpp
@@ -13,23 +13,29 @@ using You::QueryEngine::GetTask;
 using You::QueryEngine::DeleteTask;
 using You::QueryEngine::executeQuery;
 
+using boost::gregorian::date;
+using boost::gregorian::max_date_time;
+
 TEST_CLASS(QueryEngineTest) {
-	/// Test whether NLP's can construct
-	/// a query and it can be responded correctly
-	TEST_METHOD(constructQueryForNLP) {
-		const Task::Description desc = L"Learn Haskell Lens";
-		using boost::gregorian::date;
-		using boost::gregorian::max_date_time;
-		const Task::Time dead = date(max_date_time);
-		const Task::Priority prio = Task::Priority::IMPORTANT;
-		const Task::Dependencies dep = { 1, 2, 3 };
+	const Task::Description desc = L"Learn Haskell Lens";
+	const Task::Time dead = date(max_date_time);
+	const Task::Priority prio = Task::Priority::IMPORTANT;
+	const Task::Dependencies dep = Task::Dependencies();
+
+	TEST_METHOD(constructAddTaskQuery) {
 		auto resp = executeQuery(AddTask(desc, dead, prio, dep));
 		Assert::AreEqual(boost::get<std::wstring>(resp),
 			std::wstring(L"ADD"));
-		resp = executeQuery(GetTask(42));
+	}
+
+	TEST_METHOD(constructGetTaskQuery) {
+		auto resp = executeQuery(GetTask(42));
 		Assert::AreEqual(boost::get<std::wstring>(resp),
 			std::wstring(L"GET"));
-		resp = executeQuery(DeleteTask(42));
+	}
+
+	TEST_METHOD(constructDeleteTaskQuery) {
+		auto resp = executeQuery(DeleteTask(42));
 		Assert::AreEqual(boost::get<std::wstring>(resp),
 			std::wstring(L"DELETE"));
 	}
