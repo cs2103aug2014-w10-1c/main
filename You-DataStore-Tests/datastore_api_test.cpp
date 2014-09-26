f314df3ff213aa81dce14566467b0192ff6693fc
Diga Widyaprana <digawp@gmail.com>
2014-09-26 11:47:11 +0800

    Rename incomplete test on datastore to DataStoreDummyTest

--- You-DataStore-Tests/datastore_api_test.cpp
+++ You-DataStore-Tests/datastore_api_test.cpp
@@ -8,7 +8,7 @@ using Assert = Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 namespace YouDataStoreTests {
 TEST_CLASS(DataStoreApiTest) {
 public:
-	TEST_METHOD(Post_EmptyTask_Test) {
+	TEST_METHOD(DataStoreDummyTest) {
 		// You::DataStore::DataStore sut;
 		// sut.post(std::unordered_map<std::wstring, std::wstring>());
 		Assert::IsTrue(true);
