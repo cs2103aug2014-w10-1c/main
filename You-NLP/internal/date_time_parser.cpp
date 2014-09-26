63fb42e1680318aa9b67f6b3a0f49ac3f8a487c4
Joel Low <joel@joelsplace.sg>
2014-09-26 13:29:22 +0800

    Make the DateTimeParser a full parser.

---
+++ You-NLP/internal/date_time_parser.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "../exceptions/parser_exception.h"
+#include "date_time_parser.h"
+
+namespace You {
+namespace NLP {
+namespace Internal {
+
+using ptime = boost::posix_time::ptime;
+namespace qi = boost::spirit::qi;
+
+ptime DateTimeParser::parse(const StringType& string) {
+	ptime result;
+	bool success = qi::phrase_parse(
+		begin(string),
+		end(string),
+		DateTimeParser(),
+		ParserSkipperType(),
+		result);
+
+	if (success) {
+		return result;
+	} else {
+		throw ParserException();
+	}
+}
+
+}  // namespace Internal
+}  // namespace NLP
+}  // namespace You
--- You-NLP/internal/date_time_parser.h
+++ You-NLP/internal/date_time_parser.h
@@ -3,23 +3,39 @@
 #define YOU_NLP_INTERNAL_DATE_TIME_PARSER_H_
 
 #include <boost/date_time/gregorian/gregorian_types.hpp>
-#include "query_parser.h"
+#include "parser.h"
 
 namespace You {
 namespace NLP {
 namespace Internal {
 
-using date = boost::gregorian::date;
-using ptime = boost::posix_time::ptime;
-
 /// The query parser that recognises our input syntax.
-class DateTimeParser {
+class DateTimeParser : public boost::spirit::qi::grammar<
+	ParserIteratorType,
+	boost::posix_time::ptime(),
+	ParserSkipperType> {
+public:
+	/// The type of the iterator used in this grammar.
+	typedef ParserIteratorType IteratorType;
+
+	/// The type of the skipper used in this grammar.
+	typedef ParserSkipperType SkipperType;
+
+	/// The type of the lexeme buffer provided in a lexing semantic action.
+	typedef std::vector<ParserCharEncoding::char_type> LexemeType;
+
+	/// The type of input strings accepted by this parser.
+	typedef std::basic_string<ParserCharEncoding::char_type> StringType;
+
 public:
 	/// Parses the given input string into date.
 	///
 	/// \param[in] string The string to parse.
 	/// \return The date/time representing the string.
-	static ptime parse(const std::wstring& string);
+	static boost::posix_time::ptime parse(const StringType& string);
+
+private:
+	DateTimeParser();
 };
 
 }  // namespace Internal
