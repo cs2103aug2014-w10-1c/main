17de38efdef52050de4216861ad7a2e3ef4ddcf5
Joel Low <joel@joelsplace.sg>
2014-09-27 11:04:49 +0800

    Suppress most of the harmless VC warnings generated within Boost.

--- You-NLP/stdafx.h
+++ You-NLP/stdafx.h
@@ -12,13 +12,17 @@
 #include <string>
 #include <stdexcept>
 
+#pragma warning(disable: 4503) // Boost is too awesome.
+#pragma warning(push)
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/format.hpp>
 #include <boost/fusion/adapted.hpp>
 #include <boost/lexical_cast.hpp>
+#pragma warning(disable: 4127 4100)
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix.hpp>
 #include <boost/variant.hpp>
+#pragma warning(pop)
 
 #endif  // YOU_NLP_STDAFX_H_
