d7ad876d7f4760f3fd6b830ddcb087ac7af4d813
Evan Sebastian <evans@comp.nus.edu.sg>
2014-09-27 23:15:25 +0800

    Refactor test case a little bit

--- You-QueryEngine-Tests/task_serializer_tests.cpp
+++ You-QueryEngine-Tests/task_serializer_tests.cpp
@@ -11,39 +11,33 @@ using Assert = Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 
 namespace UnitTests {
 
+using boost::posix_time::ptime;
+using boost::posix_time::time_duration;
+using boost::gregorian::date;
+using boost::gregorian::greg_month;
+
 using Task = You::QueryEngine::Task;
 using TaskBuilder = You::QueryEngine::Internal::TaskBuilder;
 using TaskSerializer = You::QueryEngine::Internal::TaskSerializer;
 
 TEST_CLASS(TaskBuilderTest) {
 public:
-	/// Should be able to serialize a simple task.
-	TEST_METHOD(serializeSimpleTask) {
-		const Task::Description desc = L"Learn Haskell Lens";
-		Task t = TaskBuilder::get().description(desc);
-		auto serialized = TaskSerializer::serialize(t);
-		Assert::AreEqual(serialized[TaskSerializer::KEY_DESCRIPTION],
-			desc);
+	static Task getMockTask() {
+		Task::Description desc = L"Learn Haskell Lens";
+		Task::Time dead = ptime(date(2002, 1, 10),
+			time_duration(1, 2, 3));
+		Task::Dependencies dep = { 1, 2, 3 };
+		Task::Priority prio = Task::Priority::IMPORTANT;
+		return TaskBuilder::get().description(desc).deadline(dead).
+			priority(prio).dependencies(dep);
 	}
 
 	/// Should be able to deserialize a complex task.
-	TEST_METHOD(serializeComplexTask) {
-		const Task::Description desc = L"Learn Haskell Lens";
-		using boost::posix_time::ptime;
-		using boost::posix_time::time_duration;
-		using boost::gregorian::date;
-		using boost::gregorian::greg_month;
-		const Task::Time dead = ptime(date(2002, 1, 10),
-			time_duration(1, 2, 3));
-		const Task::Dependencies dep = { 1, 2, 3 };
-		const Task::Priority prio = Task::Priority::IMPORTANT;
-		Task task = TaskBuilder::get()
-			.description(desc)
-			.deadline(dead)
-			.priority(prio)
-			.dependencies(dep);
+	TEST_METHOD(serializeTask) {
+		Task task = getMockTask();
 		auto serialized = TaskSerializer::serialize(task);
-		Assert::AreEqual(serialized[TaskSerializer::KEY_DESCRIPTION], desc);
+		Assert::AreEqual(serialized[TaskSerializer::KEY_DESCRIPTION],
+			task.getDescription());
 		Assert::AreEqual(serialized[TaskSerializer::KEY_PRIORITY],
 			std::wstring(L"important"));
 		Assert::AreEqual(serialized[TaskSerializer::KEY_DEPENDENCIES],
