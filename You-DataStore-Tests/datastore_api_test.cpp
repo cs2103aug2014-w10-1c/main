1ed0628a647e9125bd7300e27cca37729bff89f2
Diga Widyaprana <digawp@gmail.com>
2014-09-28 17:52:54 +0800

    Add unit test for post method

--- You-DataStore-Tests/datastore_api_test.cpp
+++ You-DataStore-Tests/datastore_api_test.cpp
@@ -4,14 +4,22 @@
 #include "datastore.h"
 
 using Assert = Microsoft::VisualStudio::CppUnitTestFramework::Assert;
+using DataStore = You::DataStore::DataStore;
 
 namespace YouDataStoreTests {
 TEST_CLASS(DataStoreApiTest) {
 public:
-	TEST_METHOD(DataStoreDummyTest) {
-		// You::DataStore::DataStore sut;
-		// sut.post(std::unordered_map<std::wstring, std::wstring>());
-		Assert::IsTrue(true);
+	TEST_METHOD(DataStore_Post_Basic_Test) {
+		You::DataStore::DataStore sut;
+		bool result = sut.post(0, DataStore::STask());
+		Assert::IsTrue(result);
+	}
+
+	TEST_METHOD(DataStore_Post_DuplicateId_Test) {
+		You::DataStore::DataStore sut;
+		sut.post(0, DataStore::STask());
+		bool result = sut.post(0, DataStore::STask());
+		Assert::IsFalse(result);
 	}
 };
 }  // namespace YouDataStoreTests
