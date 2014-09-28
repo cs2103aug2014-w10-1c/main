576dc8d16e20c2f74957079fed8b9af2998abfb6
Diga Widyaprana <digawp@gmail.com>
2014-09-28 20:54:54 +0800

    Add another test for put method

--- You-DataStore-Tests/datastore_api_test.cpp
+++ You-DataStore-Tests/datastore_api_test.cpp
@@ -26,8 +26,10 @@ public:
 	DataStore sut;
 
 	TEST_METHOD(DataStore_Post_Basic_Test) {
+		// Assert::IsTrue(sut.document.empty());
 		bool result = sut.post(0, DataStore::STask());
 		Assert::IsTrue(result);
+		// Assert::IsFalse(sut.document.empty());
 	}
 
 	TEST_METHOD(DataStore_Post_DuplicateId_Test) {
@@ -38,8 +40,29 @@ public:
 
 	TEST_METHOD(DataStore_Put_Basic_Test) {
 		sut.post(0, DataStore::STask());
-		bool result = sut.put(0, DataStore::STask());
+		bool result = sut.put(0, stask);
 		Assert::IsTrue(result);
+		// Check for the node being edited
+	}
+
+	TEST_METHOD(DataStore_Put_FalseId_Test) {
+		sut.post(0, DataStore::STask());
+		bool result = sut.put(1, DataStore::STask());
+		Assert::IsFalse(result);
+		// Check for the node being edited
+	}
+
+	TEST_METHOD(DataStore_Get_Basic_Test) {
+		sut.post(0, stask);
+		boost::variant<bool, DataStore::STask> response = sut.get(0);
+		bool status = boost::get<bool>(response);
+		Assert::IsTrue(status);
+		DataStore::STask task = boost::get<DataStore::STask>(response);
+		Assert::AreEqual(stask.at(TASK_ID), task[TASK_ID]);
+		Assert::AreEqual(stask.at(DESCRIPTION), task[DESCRIPTION]);
+		Assert::AreEqual(stask.at(DEADLINE), task[DEADLINE]);
+		Assert::AreEqual(stask.at(PRIORITY), task[PRIORITY]);
+		Assert::AreEqual(stask.at(DEPENDENCIES), task[DEPENDENCIES]);
 	}
 };
 }  // namespace YouDataStoreTests
