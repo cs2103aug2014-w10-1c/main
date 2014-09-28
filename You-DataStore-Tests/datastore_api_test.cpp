a314b98f1daf899e9562a00a39ceb2b19105d274
Diga Widyaprana <digawp@gmail.com>
2014-09-28 18:51:15 +0800

    Add test for put method

--- You-DataStore-Tests/datastore_api_test.cpp
+++ You-DataStore-Tests/datastore_api_test.cpp
@@ -9,17 +9,23 @@ using DataStore = You::DataStore::DataStore;
 namespace YouDataStoreTests {
 TEST_CLASS(DataStoreApiTest) {
 public:
+	DataStore sut;
+
 	TEST_METHOD(DataStore_Post_Basic_Test) {
-		You::DataStore::DataStore sut;
 		bool result = sut.post(0, DataStore::STask());
 		Assert::IsTrue(result);
 	}
 
 	TEST_METHOD(DataStore_Post_DuplicateId_Test) {
-		You::DataStore::DataStore sut;
 		sut.post(0, DataStore::STask());
 		bool result = sut.post(0, DataStore::STask());
 		Assert::IsFalse(result);
+	}
+
+	TEST_METHOD(DataStore_Put_Basic_Test) {
+		sut.post(0, DataStore::STask());
+		bool result = sut.put(0, DataStore::STask());
+		Assert::IsTrue(result);
 	}
 };
 }  // namespace YouDataStoreTests
