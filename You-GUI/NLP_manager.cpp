1d43985b38fcbc3977e1ec8d9e751e2ce0b62558
angathorion <tanghuansong@gmail.com>
2014-10-01 08:38:22 +0800

    Added project ref

--- You-GUI/NLP_manager.cpp
+++ You-GUI/NLP_manager.cpp
@@ -12,16 +12,18 @@ void YouMainGUI::NLPManager::setup() {
     connect(parentGUI->ui.commandEnterButton,
 		SIGNAL(clicked()), this, SLOT(commandEnterButtonClicked()));
 	// To fix
-	// You::NLP::Controller::get().query(L"",
-	// You::NLP::Controller::Context::DEFAULT);
+	You::NLP::Controller::get().query(L"",
+		You::NLP::Controller::Context::DEFAULT);
 }
 
 You::NLP::Result YouMainGUI::NLPManager::queryNLP() {
 	/// Feed query and context into NLP engine
 	// To fix
-	// std::wstring inputString =
-	// parentGUI->ui.commandInputBox->text().toStdWString();
+	std::wstring inputString =
+		parentGUI->ui.commandInputBox->text().toStdWString();
 	You::NLP::Result result;
+	/// You::NLP::Controller::get().query(L"",
+	///	You::NLP::Controller::Context(currentResult));
 	return result;
 }
 
