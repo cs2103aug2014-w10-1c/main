b17786f04498879792f50fd3d3a9eb3db885cc27
Joel Low <joel@joelsplace.sg>
2014-10-05 16:57:22 +0800

    Just blow up when trying to access the default context. This way the client code does not need to check for default contexts.
    Conflicts:
    	You-Controller/controller_context.cpp

--- You-Controller-Tests/controller_context_tests.cpp
+++ You-Controller-Tests/controller_context_tests.cpp
@@ -1,12 +1,15 @@
 //@author A0097630B
 #include "stdafx.h"
 #include "controller_context.h"
+#include "exception.h"
 
 using Assert = Microsoft::VisualStudio::CppUnitTestFramework::Assert;
 
 namespace You {
 namespace Controller {
 namespace UnitTests {
+
+using You::NLP::ContextRequiredException;
 
 TEST_CLASS(ControllerContextTests) {
 public:
@@ -18,6 +21,29 @@ public:
 		TaskList list;
 		Controller::Context controllerContext(list);
 		Assert::IsFalse(controllerContext.isDefault());
+	}
+
+	TEST_METHOD(throwsContextRequiredExceptionWhenDefaultIsUsed) {
+		Assert::ExpectException<ContextRequiredException>([]() {
+			Controller::Context::DEFAULT.at(0);
+		}, L"Default context throws exception when used.");
+
+		Assert::ExpectException<ContextRequiredException>([]() {
+			Controller::Context::DEFAULT[0];
+		}, L"Default context throws exception when used.");
+	}
+
+	TEST_METHOD(atThrowsOutOfRangeWhenUsedWithInvalidIndex) {
+		TaskList list;
+		Controller::Context controllerContext(list);
+
+		Assert::ExpectException<std::out_of_range>([&]() {
+			controllerContext.at(0);
+		}, L"at throws exception when used with invalid index.");
+	}
+
+	TEST_METHOD(atAndArrayAccessReturnsCorrectItem) {
+		
 	}
 };
 
--- You-Controller/controller_context.h
+++ You-Controller/controller_context.h
@@ -21,23 +21,21 @@ public:
 	/// Task List conversion constructor.
 	Context(const TaskList& taskList);  // NOLINT(runtime/explicit)
 
-	/// Checks if the given context is the default context.
+	/// Get the task at the given offset.
 	///
-	/// \return true if the current context is the default context.
-	bool isDefault() const;
+	/// \param[in] offset The offset to retrieve.
+	/// \exception ContextRequiredException When the given context is the
+	///                                     default context.
+	/// \exception std::out_of_range When the offset is larger than the number
+	///                              of tasks present.
+	const Task& at(size_t offset) const;
 
-	/// Checks if the given context is a task list context.
+	/// Get the task at the given offset.
 	///
-	/// \return true if the current context is a task list context.
-	/// \ref getTaskList
-	bool isTaskList() const;
-
-	/// Gets the task list if the current context is a task list
-	/// context.
-	///
-	/// \return The task list if this is a task list context. Behaviour
-	///         is undefined otherwise.
-	const TaskList& getTaskList() const;
+	/// \param[in] offset The offset to retrieve.
+	/// \exception ContextRequiredException When the given context is the
+	///                                     default context.
+	const Task& operator[](size_t offset) const;
 
 public:
 	/// This is the global default context.
@@ -49,6 +47,11 @@ private:
 	///
 	/// \see Context::DEFAULT
 	Context();
+
+	/// Checks if the given context is the default context.
+	///
+	/// \return true if the current context is the default context.
+	bool isDefault() const;
 
 private:
 	/// The actual context.
