60a866307ff65434bf112b747f52348f29344179
Diga Widyaprana <digawp@gmail.com>
2014-10-07 00:57:55 +0800

    Add unit test class declaration and friend class it in DataStore

--- You-DataStore/datastore.h
+++ You-DataStore/datastore.h
@@ -9,9 +9,10 @@
 
 namespace You {
 namespace DataStore {
-namespace UnitTests {}
+namespace UnitTests { class DataStoreApiTest; }
 
 class DataStore {
+	friend class UnitTests::DataStoreApiTest;
 public:
 	Transaction& begin();
 
