580cc435808f83633892f76abb4147c16f6cf810
Evan Sebastian <evans@comp.nus.edu.sg>
2014-10-06 14:18:10 +0800

    Add the stub for task graph

---
+++ You-QueryEngine/internal/task_graph.h
@@ -0,0 +1,24 @@
+/// \file task_graph.h
+/// Defines the TaskGraph class
+/// \author A0112054Y
+
+#pragma once
+#ifndef YOU_QUERYENGINE_INTERNAL_TASK_GRAPH_H_
+#define YOU_QUERYENGINE_INTERNAL_TASK_GRAPH_H_
+
+#include <vector>
+#include "../api.h"
+
+namespace You {
+namespace QueryEngine {
+namespace Internal {
+
+/// Defines the task dependency graph
+class TaskGraph {
+};
+
+}  // namespace Internal
+}  // namespace QueryEngine
+}  // namespace You
+
+#endif  // YOU_QUERYENGINE_INTERNAL_TASK_GRAPH_H_
