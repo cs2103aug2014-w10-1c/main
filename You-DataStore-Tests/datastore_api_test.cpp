093afe948e73e70d213b3f8c5f96b1abe45ce23a
Diga Widyaprana <digawp@gmail.com>
2014-10-07 00:57:36 +0800

    Create unit test file for datastore api

---
+++ You-DataStore-Tests/datastore_api_test.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using Assert = Microsoft::VisualStudio::CppUnitTestFramework::Assert;
+
+namespace You {
+namespace DataStore {
+namespace UnitTests {
+TEST_CLASS(DataStoreApiTest) {
+public:
+	//TEST_METHOD(TestMethod1) {
+	//	// TODO: Your test code here
+	//}
+
+};
+
+}  // namespace UnitTests
+}  // namespace DataStore
+}  // namespace You
