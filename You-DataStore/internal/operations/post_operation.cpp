e55e210c3c2290be79e90d153943a6aeeb7526a4
Diga Widyaprana <digawp@gmail.com>
2014-10-06 15:59:50 +0800

    Add skeleton for PostOperation

---
+++ You-DataStore/internal/operations/post_operation.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "post_operation.h"
+
+namespace You {
+namespace DataStore {
+namespace Internal {
+
+}  // namespace Internal
+}   // namespace DataStore
+}   // namespace You
