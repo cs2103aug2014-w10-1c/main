3a7937af55780e9b2d38ff234d8ddbda90578313
Joel Low <joel@joelsplace.sg>
2014-10-07 10:36:02 +0800

    Add more files to the PCH for faster compiles.

--- You-GUI/stdafx.h
+++ You-GUI/stdafx.h
@@ -2,5 +2,14 @@
 #pragma once
 #ifndef YOU_GUI_STDAFX_H_
 #define YOU_GUI_STDAFX_H_
+
+#include <memory>
+#include <QApplication>
+#include <QList>
+#include <QWidget>
 #include <QtWidgets>
+
+#include <boost/date_time/gregorian/greg_month.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #endif  // YOU_GUI_STDAFX_H_
