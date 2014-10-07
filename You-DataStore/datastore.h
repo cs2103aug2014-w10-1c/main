daf2f1f294c5ea583fdf33c98c3e0fc865d80985
Diga Widyaprana <digawp@gmail.com>
2014-10-07 21:49:47 +0800

    Document DataStore

--- You-DataStore/datastore.h
+++ You-DataStore/datastore.h
@@ -14,22 +14,34 @@ namespace UnitTests { class DataStoreApiTest; }
 class DataStore {
 	friend class UnitTests::DataStoreApiTest;
 public:
+	/// Start a new transaction
+	/// It will do a busy wait if there is another transaction taking place
+	/// \return a reference to a new Transaction
 	Transaction& begin();
 
+	/// Get the instance of DataStore
 	static DataStore& get();
 
 	// Modifying methods
+	/// Push a post operation to operation queue
 	void post(TaskId, const SerializedTask&);
+	/// Push a put operation to operation queue
 	void put(TaskId, const SerializedTask&);
+	/// Push an erase operation to operation queue
 	void erase(TaskId);
 
+	/// Get all tasks
+	/// \return a vector of SerializedTasks
 	std::vector<SerializedTask> getAllTask();
 
+	/// Notify DataStore that a transaction is committed
+	/// To be used only by Transaction object
 	void notify();
 
 private:
+	/// Boolean to ensure mutual exclusion
 	bool isServing = false;
-	std::shared_ptr<Transaction> activeTransaction;
+
 	std::stack<std::shared_ptr<Transaction>> transactionStack;
 };
 }  // namespace DataStore
