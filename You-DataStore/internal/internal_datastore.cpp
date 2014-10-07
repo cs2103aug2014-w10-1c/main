369499a5d2e8b5daa836760b372acf80ed962b66
Diga Widyaprana <digawp@gmail.com>
2014-10-08 02:24:34 +0800

    Modify implementation of loadData

--- You-DataStore/internal/internal_datastore.cpp
+++ You-DataStore/internal/internal_datastore.cpp
@@ -67,11 +67,15 @@ bool InternalDataStore::saveData() {
 }
 
 void InternalDataStore::loadData() {
-	pugi::xml_parse_result status = document.load_file(FILE_PATH.c_str());
+	bool isInitialized =
+		document.first_child().type() != pugi::xml_node_type::node_null;
+	if (!isInitialized) {
+		pugi::xml_parse_result status = document.load_file(FILE_PATH.c_str());
 
-	// Not sure if the if block below is necessary
-	if (status == pugi::xml_parse_status::status_file_not_found) {
-		document.reset();
+		// Not sure if the if block below is necessary
+		if (status == pugi::xml_parse_status::status_file_not_found) {
+			document.reset();
+		}
 	}
 }
 
