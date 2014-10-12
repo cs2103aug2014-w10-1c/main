df6a3cc93631343531e6d24548ac024f04f710cd
angathorion <tanghuansong@gmail.com>
2014-10-12 15:49:51 +0800

    Finally got some testing working.
    Conflicts:
    	You-GUI-Tests/you_gui_tests.cpp

---
+++ You-GUI-Tests/test_tests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <QtTest/QtTest>
+#include "You-GUI\you_main_gui.h"
+#include "You-GUI\system_tray_manager.h"
+class TestYouMainGUI : public QObject
+{
+	Q_OBJECT
+		private slots:
+	void testGUI();
+};
+
+void TestYouMainGUI::testGUI() {
+	YouMainGUI w;
+	YouMainGUI::SystemTrayManager stm(&w);
+	stm.setup();
+	QVERIFY(w.isVisible());
+}
+
+#include "You-GUI-Tests\GeneratedFiles\Debug\test_tests.moc"
+QTEST_MAIN(TestYouMainGUI)
