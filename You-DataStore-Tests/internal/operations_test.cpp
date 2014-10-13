d6130adabbd67f8ffbd18e3a5df1ae19472a4a94
Diga Widyaprana <digawp@gmail.com>
2014-10-14 00:49:42 +0800

    Add unit test for Operation classes

--- You-DataStore-Tests/internal/operations_test.cpp
+++ You-DataStore-Tests/internal/operations_test.cpp
@@ -14,13 +14,129 @@ namespace DataStore {
 namespace UnitTests {
 
 /// Unit Test Class for DataStore class
-TEST_CLASS(OperationsTest) {
+TEST_CLASS(DataStoreOperationsTest) {
 public:
-	/// Basic test for retrieving a task
-	TEST_METHOD(postNewTask) {
-		Internal::PostOperation post();
+	TEST_METHOD(serializeOperation) {
+		pugi::xml_document document;
+		pugi::xml_node taskNode = document.append_child();
+		Internal::SerializationOperation::serialize(task1, taskNode);
+
+		Assert::AreEqual(L"0",
+			taskNode.child(TASK_ID.c_str()).child_value());
+		Assert::AreEqual(L"bla bla",
+			taskNode.child(DESCRIPTION.c_str()).child_value());
+		Assert::AreEqual(L"xxxxxx",
+			taskNode.child(DEADLINE.c_str()).child_value());
+		Assert::AreEqual(L"urgent",
+			taskNode.child(PRIORITY.c_str()).child_value());
+		Assert::AreEqual(L"12345",
+			taskNode.child(DEPENDENCIES.c_str()).child_value());
 	}
 
+	TEST_METHOD(deserializeOperation) {
+		pugi::xml_document document;
+		pugi::xml_node taskNode = document.append_child();
+		taskNode.append_child(L"elementName").
+			append_child(pugi::xml_node_type::node_pcdata).set_value(L"pcdata");
+		SerializedTask task =
+			Internal::SerializationOperation::deserialize(taskNode);
+		Assert::AreEqual(static_cast<Value>(L"pcdata"), task.at(L"elementName"));
+	}
+
+	TEST_METHOD(postWithNewId) {
+		pugi::xml_document document;
+		Assert::IsTrue(document.first_child().empty());
+
+		Internal::PostOperation post(0, task1);
+		bool status = post.run(document);
+		Assert::IsTrue(status);
+
+		// Check the content
+		pugi::xml_node taskNode = document.child(L"task");
+		Assert::AreEqual(L"0",
+			taskNode.child(TASK_ID.c_str()).child_value());
+		Assert::AreEqual(L"bla bla",
+			taskNode.child(DESCRIPTION.c_str()).child_value());
+		Assert::AreEqual(L"xxxxxx",
+			taskNode.child(DEADLINE.c_str()).child_value());
+		Assert::AreEqual(L"urgent",
+			taskNode.child(PRIORITY.c_str()).child_value());
+		Assert::AreEqual(L"12345",
+			taskNode.child(DEPENDENCIES.c_str()).child_value());
+	}
+
+	TEST_METHOD(postWithUsedId) {
+		pugi::xml_document document;
+		pugi::xml_node node = document.append_child(L"task");
+		node.append_attribute(L"id").set_value(L"0");
+
+		Internal::PostOperation post(0, task1);
+		bool status = post.run(document);
+		Assert::IsFalse(status);
+		
+		// Check the content
+		pugi::xml_node taskNode = document.child(L"task");
+		Assert::IsTrue(taskNode.first_child().empty());
+	}
+
+	TEST_METHOD(putWithExistingId) {
+		pugi::xml_document document;
+		pugi::xml_node node = document.append_child(L"task");
+		node.append_attribute(L"id").set_value(L"0");
+
+		Internal::PutOperation put(0, task1);
+		bool status = put.run(document);
+		Assert::IsTrue(status);
+
+		// Check the content
+		pugi::xml_node taskNode = document.child(L"task");
+		Assert::AreEqual(L"0",
+			taskNode.child(TASK_ID.c_str()).child_value());
+		Assert::AreEqual(L"bla bla",
+			taskNode.child(DESCRIPTION.c_str()).child_value());
+		Assert::AreEqual(L"xxxxxx",
+			taskNode.child(DEADLINE.c_str()).child_value());
+		Assert::AreEqual(L"urgent",
+			taskNode.child(PRIORITY.c_str()).child_value());
+		Assert::AreEqual(L"12345",
+			taskNode.child(DEPENDENCIES.c_str()).child_value());
+	}
+
+	TEST_METHOD(putNonExistentId) {
+		pugi::xml_document document;
+		pugi::xml_node node = document.append_child(L"task");
+		node.append_attribute(L"id").set_value(L"0");
+
+		Internal::PutOperation put(1, task1);
+		bool status = put.run(document);
+		Assert::IsFalse(status);
+
+		// Check the content
+		pugi::xml_node taskNode = document.child(L"task");
+		Assert::AreEqual(L"0", taskNode.attribute(L"id").value());
+	}
+
+	TEST_METHOD(eraseExistingId) {
+		pugi::xml_document document;
+		pugi::xml_node node = document.append_child(L"task");
+		node.append_attribute(L"id").set_value(L"0");
+
+		Internal::EraseOperation erase(0);
+		bool status = erase.run(document);
+		Assert::IsTrue(status);
+		Assert::IsTrue(document.first_child().empty());
+	}
+
+	TEST_METHOD(eraseNonExistentId) {
+		pugi::xml_document document;
+		pugi::xml_node node = document.append_child(L"task");
+		node.append_attribute(L"id").set_value(L"0");
+
+		Internal::EraseOperation erase(1);
+		bool status = erase.run(document);
+		Assert::IsFalse(status);
+		Assert::IsFalse(document.first_child().empty());
+	}
 };
 }  // namespace UnitTests
 }  // namespace DataStore
