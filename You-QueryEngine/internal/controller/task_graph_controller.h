de28c5bfbf69deba1d467e32c32af94f82f29699
Evan Sebastian <evans@comp.nus.edu.sg>
2014-10-20 01:05:29 +0800

    Stub for connect children

--- You-QueryEngine/internal/controller/task_graph_controller.h
+++ You-QueryEngine/internal/controller/task_graph_controller.h
@@ -24,12 +24,14 @@ public:
 	/// Add a task to the graph if it is not exist.
 	/// May throw CircularDependencyException if it
 	/// causes circular dependency.
+	/// \param [out] graph The graph to be modified.
 	/// \param [in] task The task to be added.
 	/// \return true if the task is added, false otherwise.
 	static void addTask(TaskGraph& graph, const Task& task);
 
 	/// Delete a task from the graph.
 	/// May throw \ref Exception::TaskNotFoundException
+	/// \param [out] graph The graph to be modified.
 	/// \param [in] id The id of the to-be deleted task
 	/// \return true if success, false otherwise.
 	static void deleteTask(TaskGraph& graph, const Task::ID id);
@@ -38,11 +40,28 @@ public:
 	/// Rebuilds the graph if there is a dependency change.
 	/// May throw \ref Exception::TaskNotFoundException if trying
 	/// to update non existent task.
+	/// \param [out] graph The graph to be modified.
 	/// \param [in] task The updated task
 	static void updateTask(TaskGraph& graph, const Task& task);
 
 	/// Rebuild the graph based on task table.
 	static void rebuildGraph(TaskGraph& graph);
+
+private:
+	/// Make task of id parent depends on all task in children.
+	/// Calls connectChild on each task.
+	/// \param [out] graph The graph to be modified.
+	/// \param [in] parent The id of the parent task.
+	static void connectChildren(TaskGraph& graph, const Task::ID parent);
+
+	/// Make task of id parent depends or sub on child.
+	/// Connect an edge from the child to the parent.
+	/// No-op if already connected.
+	/// \param [out] graph The graph to be modified.
+	/// \param [in] parent The id of the parent vertex.
+	/// \param [in] child The id of the child vertex.
+	static void connectChild(TaskGraph& graph, const Task::ID parent,
+		const Task::ID child);
 };
 
 }  // namespace Internal
