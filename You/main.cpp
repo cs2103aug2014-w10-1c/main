df9ea37a0ab82d4e2fbe0c173c1073d2c20d0944
angathorion <tanghuansong@gmail.com>
2014-10-20 23:59:31 +0800

    Fixed single-application instance issue using QSharedMemory since the single application class is deprecated

--- You/main.cpp
+++ You/main.cpp
@@ -1,8 +1,14 @@
+//@author A0094446X
 #include "stdafx.h"
 #include <QtWidgets/QApplication>
 #include "../You-GUI/main_window.h"
 
 int main(int argc, char *argv[]) {
+	QSharedMemory shared("QtApplicationUniqueInstanceYouGUI");
+	if (shared.create(512, QSharedMemory::ReadWrite) == false) {
+		exit(0);
+	}
+
 	QApplication a(argc, argv);
 	You::GUI::MainWindow w;
 	w.show();
