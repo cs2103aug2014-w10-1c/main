c0eb2b8f550cb841e9bc63a7db05eef88610c404
angathorion <tanghuansong@gmail.com>
2014-10-21 23:15:25 +0800

    Fixed unnecessary include.

--- You-GUI/window_title.h
+++ You-GUI/window_title.h
@@ -2,7 +2,6 @@
 #pragma once
 #ifndef YOU_GUI_WINDOW_TITLE_H_
 #define YOU_GUI_WINDOW_TITLE_H_
-#include "stdafx.h"
 const std::wstring WINDOW_TITLE =
 L"\u7528 (Y\u014D) - Your Task Manager Application ";
 #endif  // YOU_GUI_WINDOW_TITLE_H_
