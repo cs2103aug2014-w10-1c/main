4ea3d125030f7024c608b009646b8f106b73518f
angathorion <tanghuansong@gmail.com>
2014-10-22 14:19:12 +0800

    Modified main to improve portability

--- You/main.cpp
+++ You/main.cpp
@@ -3,9 +3,24 @@
 #include <QtWidgets/QApplication>
 #include "../You-GUI/main_window.h"
 #include "../You-GUI/window_title.h"
+
+#ifdef _WIN32
 #include "windows.h"
+int loadSingleApplication(int argc, char *argv[]);
+#else
+int loadApplication(int argc, char *argv[]);
+#endif
 
 int main(int argc, char *argv[]) {
+#ifdef _WIN32
+	return loadSingleApplication(argc, argv);
+#else
+	return loadApplication(argc, argv);
+#endif
+}
+
+#ifdef _WIN32
+int loadSingleApplication(int argc, char *argv[]) {
 	HANDLE hMutex;
 	hMutex = CreateMutex(NULL, FALSE, TEXT("YouGUIUniqueApplicationInstance"));
 	DWORD m_DwLastError = GetLastError();
@@ -20,3 +35,11 @@ int main(int argc, char *argv[]) {
 		return a.exec();
 	}
 }
+#else
+int loadApplication(int argc, char *argv[]) {
+	QApplication a(argc, argv);
+	You::GUI::MainWindow w;
+	w.show();
+	return a.exec();
+}
+#endif
