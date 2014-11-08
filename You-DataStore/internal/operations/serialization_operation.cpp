837c55c900b4cb4264cdd9818d5c53d3b6072182
Diga Widyaprana <digawp@gmail.com>
2014-11-08 14:09:21 +0800

    Minor refactoring of SerializationOperation

--- You-DataStore/internal/operations/serialization_operation.cpp
+++ You-DataStore/internal/operations/serialization_operation.cpp
@@ -6,9 +6,9 @@ namespace You {
 namespace DataStore {
 namespace Internal {
 
-void SerializationOperation::serialize(const KeyValuePairs& stask,
+void SerializationOperation::serialize(const KeyValuePairs& kvp,
 	pugi::xml_node& node) {
-	for (auto iter = stask.begin(); iter != stask.end(); ++iter) {
+	for (auto iter = kvp.begin(); iter != kvp.end(); ++iter) {
 		pugi::xml_node keyNode =
 			node.append_child(iter->first.c_str());
 		pugi::xml_node keyValue =
@@ -17,14 +17,13 @@ void SerializationOperation::serialize(const KeyValuePairs& stask,
 	}
 }
 
-KeyValuePairs SerializationOperation::deserialize(
-	const pugi::xml_node& taskNode) {
-	KeyValuePairs stask;
+KeyValuePairs SerializationOperation::deserialize(const pugi::xml_node& node) {
+	KeyValuePairs kvp;
 	for (auto iter = taskNode.begin(); iter != taskNode.end(); ++iter) {
-		stask.insert(KeyValuePair(Key(iter->name()),
+		kvp.insert(KeyValuePair(Key(iter->name()),
 			Value(iter->child_value())));
 	}
-	return stask;
+	return kvp;
 }
 
 }  // namespace Internal
